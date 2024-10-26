fe8fd4f4af6951c33c6ffbb623c2ccf7454dc924
ShipBee Technologies <123997217+Ziad-Asr@users.noreply.github.com>
2024-10-26 11:04:01 +0300

    build register and login forms using (react hoks form ) + Adding validation using (Zod)

--- Frontend/src/pages/Register.tsx
+++ Frontend/src/pages/Register.tsx
@@ -1,54 +1,64 @@
 import { useForm, SubmitHandler } from "react-hook-form";
 
-import { Heading } from "@components/common";
 import { Button, Col, Form, Row } from "react-bootstrap";
-
-type TFormInputs = {
-  firstName: string;
-  lastName: string;
-  email: string;
-  password: string;
-  confirmPassword: string;
-};
+import { zodResolver } from "@hookform/resolvers/zod";
+import { signUpSchema, signUpType } from "@validations/signUpSchema";
+import { Heading } from "@components/common";
+import { Input } from "@components/Form";
 
 const Register = () => {
-  const { register, handleSubmit } = useForm<TFormInputs>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<signUpType>({
+    mode: "onBlur",
+    resolver: zodResolver(signUpSchema),
+  });
 
-  const submitForm: SubmitHandler<TFormInputs> = (data) => {
+  const submitForm: SubmitHandler<signUpType> = (data) => {
     console.log(data);
   };
 
   return (
     <>
       <Heading title="User registration" />
-
       <Row>
         <Col md={{ span: 6, offset: 3 }}>
           <Form onSubmit={handleSubmit(submitForm)}>
-            <Form.Group className="mb-3">
-              <Form.Label>First Name</Form.Label>
-              <Form.Control type="text" {...register("firstName")} />
-            </Form.Group>
-
-            <Form.Group className="mb-3">
-              <Form.Label>Last Name</Form.Label>
-              <Form.Control type="text" {...register("lastName")} />
-            </Form.Group>
-
-            <Form.Group className="mb-3">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control type="text" {...register("email")} />
-            </Form.Group>
-
-            <Form.Group className="mb-3">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" {...register("password")} />
-            </Form.Group>
-
-            <Form.Group className="mb-3">
-              <Form.Label>Confirm Password</Form.Label>
-              <Form.Control type="password" {...register("confirmPassword")} />
-            </Form.Group>
+            <Input
+              label="First Name"
+              name="firstName"
+              register={register}
+              error={errors.firstName?.message}
+            />
+            <Input
+              label="Last Name"
+              name="lastName"
+              register={register}
+              error={errors.lastName?.message}
+            />
+            <Input
+              type="text"
+              label="Email"
+              name="email"
+              register={register}
+              error={errors.email?.message}
+            />
+            <Input
+              type="password"
+              label="Password"
+              name="password"
+              register={register}
+              error={errors.password?.message}
+            />
+            <Input
+              type="password"
+              label="Confirm Password"
+              name="confirmPassword"
+              register={register}
+              error={errors.confirmPassword?.message}
+            />
 
             <Button variant="info" type="submit" style={{ color: "white" }}>
               Submit
